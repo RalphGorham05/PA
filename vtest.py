6973beebb82bf613e93158e5cc74d00d17714b4b
RalphGorham05 <Rgorham1@gaels.iona.edu>
2016-05-20 15:44:45 -0400

    work on test to access staging

--- vtest.py
+++ vtest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import mechanize
 from mechanize import Browser
 from bs4 import BeautifulSoup
 from robobrowser import RoboBrowser
@@ -10,119 +11,142 @@ import re
 import os
 
 
-training = 'http://192.168.19.20/vportal/login.html'
-vp = 'http://vportal.creativevirtual15.com/vportal/'
 
-
-def login(site):
-	browser = webdriver.Firefox()
-	browser.get(site)
-
-	#Login creds
-	tuser = 'ralphg'
-	vuser = 'rgorham'
-	password = 'Rrrrrrrrrrrrrr83##'
-
-	#Enter user and pw
-	time.sleep(2)
-	inputUser = browser.find_element_by_name("j_username")
-	inputUser.send_keys(vuser)
-
-	inputPW = browser.find_element_by_name('j_password')
-	inputPW.send_keys(password)
-
-	#click login button
-	log = browser.find_element_by_id('btnLogin').click()
-
-	browser.execute_script("openProjectDB('#gridProjectList',3);")
-
-	return browser
+class Auto_NAT:
+        def __init__(self):
+                self.training = 'http://192.168.19.20/vportal/login.html'
+                self.vp = 'http://vportal.creativevirtual15.com/vportal/'
 
 
 
-def NAT(browser,*inputs):
-	browser.find_element_by_link_text('TEST').click()
-	browser.find_element_by_link_text('Content').click()
-	browser.find_element_by_link_text('Non Approved Data').click()
 
-	time.sleep(2.5)
-	browser.find_element_by_id('btnAsk').click()
-	time.sleep(2.5)
+        def login(self, site):
+                browser = webdriver.Firefox()
+                browser.get(site)
 
-	inputQuestion = browser.find_element_by_name("questionTxt")
-	inputQuestion.send_keys(inputs)
-	browser.execute_script('getAnswer();')
+                #Login creds
+                tuser = 'ralphg'
+                vuser = 'rgorham'
+                password = 'Rrrrrrrrrrrrrr83##'
+
+                #Enter user and pw
+                time.sleep(2)
+                inputUser = browser.find_element_by_name("j_username")
+                inputUser.send_keys(vuser)
+
+                inputPW = browser.find_element_by_name('j_password')
+                inputPW.send_keys(password)
+
+                #click login button
+                log = browser.find_element_by_id('btnLogin').click()
+
+                browser.execute_script("openProjectDB('#gridProjectList',3);")
+
+                return browser
+
+
+
+        def NAT(self, browser, *inputs):
+                browser.find_element_by_link_text('TEST').click()
+                browser.find_element_by_link_text('Content').click()
+                browser.find_element_by_link_text('Non Approved Data').click()
+
+                time.sleep(2.5)
+                browser.find_element_by_id('btnAsk').click()
+                time.sleep(2.5)
+
+                inputQuestion = browser.find_element_by_name("questionTxt")
+                inputQuestion.send_keys(inputs)
+                browser.execute_script('getAnswer();')
+                
+                # browser.find_element_by_id('btnAsk').click()
+                time.sleep(3)
+
+                browser.find_element_by_id('btnCondition').click()
+                time.sleep(3)
+
+                question = browser.find_element_by_id('nlpCondition_question')
+                cond_question = question.get_attribute('value')
+                if cond_question == '':
+                        cond_question = 'Safety Net'
+                print inputs 
+                print '------------->>>>>  %s'  % cond_question
+                print ''
+                browser.execute_script('closeActiveTab();')
 	
-	# browser.find_element_by_id('btnAsk').click()
-	time.sleep(3)
-
-	browser.find_element_by_id('btnCondition').click()
-	time.sleep(3)
-
-	question = browser.find_element_by_id('nlpCondition_question')
-	cond_question = question.get_attribute('value')
-	if cond_question == '':
-		cond_question = 'Safety Net'
-	print inputs 
-	print '------------->>>>>  %s'  % cond_question
-	print ''
-	browser.execute_script('closeActiveTab();')
-	
 
 
 
 
 
 
-def clean(fle):
-        with open(fle,'r') as f:
-                ignore = ['|','PAST INPUTS','_']
-                lst = []
-                for line in f:
-                	# print line
-                        if not any(word in line for word in ignore) and line.isdigit:
+        def clean(self, fle):
+                with open(fle,'r') as f:
+                        ignore = ['|','PAST INPUTS','_']
+                        lst = []
+                        for line in f:
                                 # print line
-                                result = ''.join([i for i in line])
-                                # print result
-                                lst.append(result)
+                                if not any(word in line for word in ignore) and line.isdigit:
+                                        # print line
+                                        result = ''.join([i for i in line])
+                                        # print result
+                                        lst.append(result)
+
+                cleaned = []
+                for l in lst:
+                        l = re.sub('^\d+\s', '', l)
+                        if len(l) >5 and l != '':
+                                cleaned.append(l)
 
 
-        cleaned = []
-        for l in lst:
-        	l = re.sub('^\d+\s', '', l)
-                if len(l) >5 and l != '':
-                        cleaned.append(l)
-
-    
-       
-
-        return cleaned
-
+                return cleaned
 
 
 
 
 
 
-try:
-	os.chdir('C:\Users\Ralph Gorham\Desktop\snapp\suggestions')
-	br = login(vp)
-	filename = 'confirmation number.txt'
-	txt = clean(filename)
+        def run(self, text):
+                try:
+                        os.chdir('C:\Users\Ralph Gorham\Desktop\snapp\suggestions')
+                        br = self.login(self.vp)
+                        filename = 'confirmation number.txt'
+                        # txt = self. clean(filename)
 
-	for line in txt:
-		NAT(br,line.strip())
-except Exception, e:
-	print str(e)
-else:
-	pass
-finally:
-	pass
+                        #for line in txt:
+                                #self. NAT(br,line.strip())
+                        self. NAT(br,text)
+                except Exception, e:
+                        print str(e)
+                else:
+                        pass
+                finally:
+                        pass
+
+                br.quit()
+                
+        def test(self):
+                staging = 'http://vastage1.creativevirtual15.com/ihgstaging/'
+                va = 'https://vastage1.creativevirtual15.com/ihgstaging/bot.html?isJSEnabled=1&channel=Root'
+                user = mechanize.Browser()
+                # user.set_handle_robots(False)
+                u = user.open(va)
+                # soup = BeatifulSoup(u)
+                # text = soup.find('input',{'id' : 'user_entry'}).get('value')
+                u.select_form(name = 'form_lingubot')
+                u['entry'] = 'how many points for a room'
+                u.submit()
+            
+                
+                
+
+a = Auto_NAT()
+a.test()
 
 
 
 
 
-br.quit()
+
 
 
